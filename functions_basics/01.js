859853ca54cea823e88568931c1fc9833337c670
Rohan-222 <145691462+Rohan-222@users.noreply.github.com>
2023-12-13 03:55:52 +0000

    functions

---
+++ functions_basics/01.js
@@ -0,0 +1,26 @@
+// n1 and n2 are parameters
+function addNumbers(n1,n2){
+    console.log(n1+n2);
+}
+
+// here 2,3 are arguments
+console.log(addNumbers(2,3));
+
+
+//another way of writing functions
+
+let addd = function(n1,n2){
+    return (n1+n2);
+    //****** invalid********
+    //sdfgfgg we should not write any thing after the return satement in the function
+}
+
+addd(2,4);
+
+
+/*
+*********BEST PRACTISE USE *******
+* always use `` for taking values of variables from functions
+* undefined , "" is false in js
+
+*/
