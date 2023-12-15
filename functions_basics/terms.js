20c7d518d2941e1d661d228af0723ff1939e8541
Rohan-222 <145691462+Rohan-222@users.noreply.github.com>
2023-12-15 05:40:13 +0000

    few changes

---
+++ functions_basics/terms.js
@@ -0,0 +1,26 @@
+//function statment
+function a(){
+    console.log("a is called");
+}
+//function Expression
+let b = function(){
+    console.log("b is called");
+}
+//function declaration == function statement
+
+
+// Anonymous func
+let d = () =>{
+    console.log("sdknfs");
+}
+
+let c =()=>{
+(console.log("executing line  in 13")); // without giving return keyword
+}
+
+c();
+
+
+// first-class function -> the ability of function to pass as argument or retrun from another function like values.
+
+
